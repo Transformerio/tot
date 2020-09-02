9d8c0f10561cae81d90d85a828c913d66c17f2e1
Transfomerio <transformermice@gmail.com>
2020-09-02 13:42:14 -0500

    testing particles

---
+++ js/app.js
@@ -0,0 +1,5 @@
+javascript
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles', '/json/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
